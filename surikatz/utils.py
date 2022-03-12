f6e5c77592e6d815b0f9000ea39cb84eadf6c0d4
Mogulz <raayanb94@gmail.com>
2022-03-12 16:19:26 +0100

    Adding of the APIClient in the utils.py file, this class will allows us to acces the resources of any API

--- surikatz/utils.py
+++ surikatz/utils.py
@@ -1,7 +1,21 @@
+#!/usr/bin/env python3
+
+from surikatz.error import APIError
 from surikatz.error import ReadError 
 from dotenv import load_dotenv
 import os
 import re
+import requests
+import whois
+import socket
+import subprocess
+import untangle
+import csv
+from rich import print
+from rich.console import Console
+import shodan
+
+console = Console()
 
 class ConfReader:
 
@@ -46,4 +60,76 @@ class Checker:
 
         else:
             return False
+
+class APIClient:
+
+    def __init__(self, basic_url,key= None, proxies=None):
+        self.base_url = basic_url
+        self._session = requests.Session()
+        if proxies:
+            self._session.proxies.update(proxies)
+            self._session.trust_env = False
+        if key:
+            self._session.headers.update(key)
+
+    def resultUrl(self, result):
+        """ A function that create a string that will return an URL link for accesssing any API
+        
+        Arguments:
+            result  -- a dictionnary that will contain the parameters that we must add to the URL in order to access the ressources
+
+        Returns:
+            A string character that will represent an URL link for accesssing the ressources of an API
+        """
+        for i, key in enumerate(result.keys()):		
+            if i ==0:
+                res = f"?{key}={result[key]}"
+            else:
+                res += ","+f"{key}={result[key]}"
+        return res 
+
+
+    def request(self, target,params= None):
+            """General-purpose function to create web requests to any API.
+
+            Arguments:
+                function  -- name of the function you want to execute
+                target    -- The endpoint for accessing the ressources we aim for.
+                params    -- dictionary of parameters for the function
+
+            Returns
+                A dictionary containing the function's results.
+
+            """
+            if params:
+                target += self.resultUrl(params)
+
+            # Send the request
+            try:
+                data = self._session.get(self.base_url + target)
+            except Exception:
+                raise APIError(f'Unable to connect to {self.base_url}')
+
+            # Check that the API key wasn't rejected
+            if data.status_code == 401:
+                try:
+                    # Return the actual error message if the API returned valid JSON
+                    error = data.json()['error']
+                except Exception as e:
+                    raise APIError(error)
+            elif data.status_code == 403:
+                raise APIError('Access denied (403 Forbidden)')
+
+            # Parse the text into JSON
+            try:
+                data = data.json()
+            except ValueError:
+                raise APIError('Unable to parse JSON response')
+
+            # Raise an exception if an error occurred
+            if type(data) == dict and 'error' in data:
+                raise APIError(data['error'])
+
+            # Return the data
+            return data
 

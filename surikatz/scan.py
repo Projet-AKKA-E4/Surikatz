00923557accca76c05cc653a751b704ce980e927
blackjack-nix <theo.peresse-gourbil@edu.esiee.fr>
2022-03-29 15:57:31 +0200

    Add nmap scan and discret scan

--- surikatz/scan.py
+++ surikatz/scan.py
@@ -2,13 +2,22 @@
     Module for performing several scans
 """
 
+import nmap
+import asyncio
+
 
 class Nmap:
     """
     Class allowing the manipulation of nmap and the parsing of its output
     """
 
+    def __init__(self):
+        self.scanner = nmap.PortScanner()
+        self.scan_result = {}
 
+    def start_nmap(self, target, args, timeout):
+        self.scan_result = self.scanner.scan(hosts=target, arguments=args, timeout=timeout)        
+        
 class HTTrak:
     """
     Class allowing the manipulation of HTTrack and the parsing of its output

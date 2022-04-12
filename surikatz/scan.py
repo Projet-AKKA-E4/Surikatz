2013f1af211f0e1bb90a13e3ff9b7457e5a8e328
alphae-nix <alphae-nix>
2022-04-12 14:25:00 +0200

    adding first wpscan part

--- surikatz/scan.py
+++ surikatz/scan.py
@@ -1,10 +1,17 @@
 """
     Module for performing several scans
 """
+#from surikatz import SURIKATZ_PATH
+# from rich import print
+# from rich.console import Console
+import json
+
+import subprocess
 
 import nmap
 import asyncio
 
+#console = Console()
 
 class Nmap:
     """
@@ -30,10 +37,52 @@ class OpenVAS:
     """
 
 
-class WPScan:
-    """
-    Class allowing the manipulation of WPScan and the parsing of its output
-    """
+
+class WpScan():
+    def __init__(self, domain, key):
+        self.domain = domain
+        self.key = key
+        self.wapplayzer_dict = wapplayzer_dict
+
+    def wappalyzer_to_wpscan(self):
+        #for plugin in wapplayzer_dict["plugins"] :
+        pass
+
+
+    def discret_wp_scan(self)->dict:
+        """
+        Allows to use wp_scan tool which returns the Wordpress website vulnerabilities
+        """
+        # scan = subprocess.run(
+        #     ["wpscan", "--url", self.domain, "--api-token", self.key, "--detection-mode", "passive"], # Passive mode
+        #     capture_output=True,
+        # )
+
+        p = Popen(["wpscan", "--url", self.domain, "--api-token", self.key, "--detection-mode", "passive", "--random-user-agent", "--output", SURIKATZ_PATH / "wpscan.json", "--format", "json"],
+                  stdout = PIPE,
+                  stderr = STDOUT,
+                  shell = True)
+        while True:
+            line = p.stdout.readline()
+            print(line)
+            if not line: break
+
+
+
+
+
+    def aggressive_wp_scan(self)->dict:
+        subprocess.run(
+            ["wpscan", "--url", self.domain, "--api-token", self.key, "--detection-mode", "aggressive", "--random-user-agent", "--output", "wpscan.json", "--format", "json"], # Passive mode
+            capture_output=True,
+        )
+
+    def dict_concatenate(self,):
+        with open('wpscan.json') as json_file:
+            wpscan_data = json.load(json_file)
+
+
+
 
 
 class Nikto:

a0de1d51d02932c2299e306f74c224f8112e09d2
alphae-nix <alphae-nix>
2022-04-20 17:11:22 +0200

    global variable in /tmp

--- surikatz/__init__.py
+++ surikatz/__init__.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pathlib import Path
 now = datetime.now()
-datenow = now.strftime("%d/%m/%Y, %H:%M:%S")
+SCAN_DATE =now.strftime("%d-%m-%Y_%H:%M:%S")
 
-SURIKATZ_PATH = Path.cwd() / datenow
+SURIKATZ_PATH = Path("/tmp") / "surikatz" / SCAN_DATE
+Path.mkdir(SURIKATZ_PATH, parents=True, exist_ok=True)

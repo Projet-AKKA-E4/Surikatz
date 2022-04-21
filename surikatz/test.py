a66d5071c5eca02ca5745030e87b0202c5f97785
Laul <46004703+alphae-nix@users.noreply.github.com>
2022-04-21 13:39:37 +0200

    Delete test.py

--- surikatz/test.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import urlparse
-
-target = ['https://www.esiee.fr', 'http://wwwprd3.esiee.fr', 'https://37.187.158.103', '37.187.158.103:443' ]
-
-for tg in target:
-    if urlparse(tg).scheme == "https":
-        print(tg)
